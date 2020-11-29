c19468621e4d8b13a5113ac66bf75cc4f42814c8
Farhan Munir <munir.farhan@gmail.com>
2020-11-29 12:10:10 +0500

    Removed initialization from iamRoleManager

--- iamRoleManager.py
+++ iamRoleManager.py
@@ -70,30 +70,3 @@ class iamRoleManager(object):
 			self._logging.create_log( "error", f"Exception in attach_iam_policy:\n{e}")
 			return None
 		return ret
-
-if __name__ == "__main__":
-	irm = iamRoleManager()
-
-	role_name = "transcoder_role"
-	policy_name = "transcoder_role_policy"
-	role_description = "Role for creating pipeline for transcoding pipeline"
-	trust_document = {
-	    "Version": "2012-10-17",
-	    "Statement": [
-	        {
-	        "Effect": "Allow",
-	        "Principal": {
-	            "Service": "elastictranscoder.amazonaws.com"
-	        },
-	        "Action": "sts:AssumeRole"
-	        }
-	    ]
-	}
-
-	role_policy = {"Version":"2008-10-17","Statement":[{"Sid":"1","Effect":"Allow","Action":["s3:Put*","s3:ListBucket","s3:*MultipartUpload*","s3:Get*"],"Resource":"*"},{"Sid":"2","Effect":"Allow","Action":"sns:Publish","Resource":"*"},{"Sid":"3","Effect":"Deny","Action":["s3:*Delete*","s3:*Policy*","sns:*Remove*","sns:*Delete*","sns:*Permission*"],"Resource":"*"}]}
-
-	resp = irm.create_iam_role(role_name, role_description, trust_document)
-	print( f"Response Create Iam Role: \n{resp} \n")
-
-	resp = irm.attach_iam_policy(role_name, policy_name, role_policy)
-	print( f"Response Attach Iam Policy: \n{resp} \n")
